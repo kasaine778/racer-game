e64a23e1c1ce3181b3a4ace3702c4101325fd5e1
kasaine778 <kasainecaleb@gmail.com>
2025-10-28 14:25:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Simple racer prototype with raycast vehicles
+import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
+import * as CANNON from "https://unpkg.com/cannon-es@0.21.0/dist/cannon-es.js";
+// ... (the rest of the raycast vehicle code would go here)
+console.log('Nova Racer Game loaded');
